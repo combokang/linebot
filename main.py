b615db2486f88bdc0f98716d77a00deaafca062b
Combo <hiromoto1995@gmail.com>
2020-12-08 01:10:46 +0800

    fix timeout error 2

--- main.py
+++ main.py
@@ -1,89 +1,38 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
 from __future__ import unicode_literals
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot import (
-    LineBotApi, WebhookParser
-)
-
 import os
-import sys
-from argparse import ArgumentParser
-
 from flask import Flask, request, abort
 from flask.logging import create_logger
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+
 
 app = Flask(__name__)
 LOG = create_logger(app)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('f7f8aac1d964528176d599b1923e3dad', None)
-channel_access_token = os.getenv(
-    'vnP2yeDIi1+tTKPF0pzTOJ5S3+L6jaOW6ybNEuT9gfLKopMtRL0wH2QzMEhtxnyNdUAvoMmROd8e1xmpkFMBH97E6NXfA25TXxc+345T6TI0QO4Xc8zxu9ZWTVaySfUsjWs7/TVl6hZ311ptx4G+kQdB04t89/1O/w1cDnyilFU=', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
 
-line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+# LINE 聊天機器人的基本資料
+line_bot_api = LineBotApi(
+    'vnP2yeDIi1+tTKPF0pzTOJ5S3+L6jaOW6ybNEuT9gfLKopMtRL0wH2QzMEhtxnyNdUAvoMmROd8e1xmpkFMBH97E6NXfA25TXxc+345T6TI0QO4Xc8zxu9ZWTVaySfUsjWs7/TVl6hZ311ptx4G+kQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('f7f8aac1d964528176d599b1923e3dad')
+
+# 接收 LINE 的資訊
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     LOG.info("Request body: " + body)
 
-    # parse webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
 
     return 'OK'
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int,
-                            default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    app.run()
