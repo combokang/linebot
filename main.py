57ef4c481bde6378dd9c6f083aa22ecb0476b8fd
Combo <hiromoto1995@gmail.com>
2020-12-09 00:13:51 +0800

    排除line官方

--- main.py
+++ main.py
@@ -43,10 +43,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+
+    # 排除line官方訊息
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 
 if __name__ == "__main__":
